01c1dfe3128b11d84cbf2a957d7b083bcdd05346
8HoLoN <8holon@gmail.com>
2015-04-24 21:41:25 +0200

    add 'use strict' statement

--- XIII.js
+++ XIII.js
@@ -1,5 +1,5 @@
 ;(function(_g){
-
+  'use strict';
   function XIII(_v,_args){
     _args = _args || {};
     if( typeof _v === 'object' ){
@@ -41,7 +41,6 @@
     var _ret = 1;
     for( var i = 0 ; i < 7 ; i++ ){
       if( _v == this.u[i]){
-        //_ret=(Math.pow(10,i-(i>>>1))*(((i+1)%2)+1))>>>1;
         _ret=(Math.pow(10,i-(i>>>1))*((~i&0x1)+1))>>>1;
       }
     }
@@ -49,64 +48,63 @@
   };
 
   XIII.prototype.r2a = function(_v) {
-    var ret = 1;
-    var last = 0;
-    var tot = 0;
+    var _ret = 1;
+    var _last = 0;
+    var _tot = 0;
 
-    ret = this.or2a(_v[0]);
-    last = ret;
+    _ret = this.or2a(_v[0]);
+    _last = _ret;
     for( var i=1,l=_v.length ; i < l ; i++ ){
 
-      ret = this.or2a(_v[i]);
+      _ret = this.or2a(_v[i]);
 
-      if( ret <= last ){
-        tot += last;
+      if( _ret <= _last ){
+        _tot += _last;
       }else{
-        tot += ret-last;
-        ret=0;
-        if( i+1<_v.length ){ret=this.or2a(_v[++i]);}
+        _tot += _ret-_last;
+        _ret=0;
+        if( i+1<_v.length ){_ret=this.or2a(_v[++i]);}
       }
 
-      last = ret;
+      _last = _ret;
     }
-    tot += last;
-    return tot;
+    _tot += _last;
+    return _tot;
   };
 
   XIII.prototype.a2r = function(_v) {
-    var ret="";
-    var nTmp=0;
+    var _ret='';
+    var _nTmp=0;
 
-    for(var i=0 ; Math.floor((_v/(Math.pow(10,i)))) > 0 ; i++){
+    for( var i=0 ; Math.floor((_v/(Math.pow(10,i)))) > 0 ; i++ ){
 
-      nTmp=Math.floor(_v/(Math.pow(10,i)))-Math.floor(_v/(Math.pow(10,i+1)))*10;
+      _nTmp=Math.floor(_v/(Math.pow(10,i)))-Math.floor(_v/(Math.pow(10,i+1)))*10;
 
-      if( nTmp>3 ){
-        if( nTmp>4 && nTmp<9 ){
+      if( _nTmp>3 ){
+        if( _nTmp>4 && _nTmp<9 ){
 
-          for(var j=0 ; j<nTmp-5 ; j++){
-            ret=this.u[i*2]+ret;
+          for( var j=0 ; j<_nTmp-5 ; j++ ){
+            _ret=this.u[i*2]+_ret;
           }
-          ret=this.u[i*2+1]+ret;
+          _ret=this.u[i*2+1]+_ret;
         }else{
-          console.log(nTmp);
-          if( nTmp==4 && i<3 ){ret=this.u[i*2]+this.u[i*2+1]+ret;}
-          else if( nTmp==9 && i<3 ){
-            ret=this.u[i*2]+this.u[i*2+2]+ret;
+          if( _nTmp==4 && i<3 ){_ret=this.u[i*2]+this.u[i*2+1]+_ret;}
+          else if( _nTmp==9 && i<3 ){
+            _ret=this.u[i*2]+this.u[i*2+2]+_ret;
           }
           else{
-            for(var j=0 ; j<nTmp ; j++){
-              ret=this.u[i*2]+ret;
+            for(var j=0 ; j<_nTmp ; j++){
+              _ret=this.u[i*2]+_ret;
             }
           }
         }
       }else{
-        for(var j=0 ; j<nTmp ; j++){
-          ret=this.u[i*2]+ret;
+        for(var j=0 ; j<_nTmp ; j++){
+          _ret=this.u[i*2]+_ret;
         }
       }
     }
-    return ret;
+    return _ret;
   };
 
 })(window);
